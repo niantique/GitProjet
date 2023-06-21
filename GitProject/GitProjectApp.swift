c6b332af1b3c691fb812eefdc7accf11104e5c97
apprenant62 <apprenant62@lyn-fmu210013.lan>
2023-06-21 13:10:00 +0200

    first commit, add gitignore and all the files

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by apprenant62 on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
